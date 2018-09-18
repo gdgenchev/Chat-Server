dbd6d2c546fa696d73d390f9316475812d8e7104
gdgenchev <gdgnechev97@gmail.com>
2018-09-18 18:21:20 +0300

    Create ChatManager class

---
+++ src/bg/chat/server/ChatManager.java
@@ -0,0 +1,29 @@
+package bg.chat.server;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatManager {
+    private static ChatManager instance = null;
+
+    private static Set<String> connectedUsernames = Collections.synchronizedSet(new HashSet<>());
+
+    public synchronized static ChatManager getInstance() { //added synchronized after some research
+        if (instance == null) {
+            instance = new ChatManager();
+        }
+        return instance;
+    }
+
+    void login(String username) {
+        connectedUsernames.add(username);
+        System.out.println("User " + username + " added!");
+    }
+
+
+    public Collection<String> getAllUsers() {
+        return connectedUsernames;
+    }
+}
