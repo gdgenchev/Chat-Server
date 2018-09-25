fb4feff79709f555c6ac3678fa38f4ada6e1ebec
gdgenchev <gdgnechev97@gmail.com>
2018-09-25 16:42:34 +0300

    Add Message for communication between client and server

--- src/bg/chat/common/Message.java
+++ src/bg/chat/common/Message.java
@@ -1,7 +1,25 @@
 package bg.chat.common;
 
-public class Message {
-    private MessageType type;
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private MsgState state;
     private Object data;
 
+    public Message(MsgState state) {
+        this.state = state;
+    }
+
+    public Message(MsgState state, Object data) {
+        this.state = state;
+        this.data = data;
+    }
+
+    public MsgState getState() {
+        return state;
+    }
+
+    public Object getData() {
+        return data;
+    }
 }
--- src/bg/chat/common/MsgState.java
+++ src/bg/chat/common/MsgState.java
@@ -1,10 +1,11 @@
 package bg.chat.common;
 
-public enum MessageType {
+public enum MsgState {
     LOGIN,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LIST_USERS,
+    LIST_CHAT_ROOMS,
     CREATE,
     CREATE_SUCCESS,
     CREATE_FAIL,
@@ -12,6 +13,9 @@ public enum MessageType {
     JOIN_SUCCESS,
     JOIN_FAIL,
     SEND_PRIVATE,
+    SEND_PRIVATE_SUCCESS,
+    SEND_PRIVATE_FAIL,
     SEND_GROUP,
-    QUIT
+    QUIT,
+
 }
