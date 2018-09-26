8ab0b8f5ea40384c64a03215af03924935141ad0
gdgenchev <gdgnechev97@gmail.com>
2018-09-26 11:23:01 +0300

    Add listeners

--- src/bg/chat/client/controller/ChatRoomController.java
+++ src/bg/chat/client/controller/ChatRoomController.java
@@ -2,19 +2,47 @@ package bg.chat.client.controller;
 
 import bg.chat.client.model.Client;
 import bg.chat.client.view.ChatRoomView;
+import bg.chat.common.Message;
+import bg.chat.common.MessageType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatRoomController {
     private Client client;
     private ChatRoomView chatRoomView;
+    private static final Logger logger = LogManager.getLogger("Chat Room Controller");
 
     public ChatRoomController(Client client, ChatRoomView chatRoomView) {
-        this.client = client;
+        this.client       = client;
         this.chatRoomView = chatRoomView;
         this.chatRoomView.setCurrentUser(client.getUsername());
-        this.chatRoomView.setCurrentChatRoom(client.getChatRoomName());
-        //Show the delete button only to the creator
-        if (chatRoomView.getCreator().equals(client.getUsername())) {
-            chatRoomView.showDeleteButton();
-        }
+        this.chatRoomView.setCurrentChatRoom(client.getChatRoom());
+
+        this.chatRoomView.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                String[] toSend = {client.getUsername(), chatRoomView.getChatRoomName()};
+                client.writeMessage(new Message(MessageType.LEAVE_ROOM, toSend));
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent windowEvent) {
+                chatRoomView.revalidate();
+                chatRoomView.repaint();
+            }
+        });
+
+        this.chatRoomView.addDeleteChatRoomListener(actionEvent ->
+                client.writeMessage(new Message(
+                        MessageType.DELETE_ROOM, chatRoomView.getChatRoomName())));
+
+        this.chatRoomView.addSendListener(actionEvent -> {
+            String[] toSend = {client.getUsername(), client.getChatRoom(),
+                    chatRoomView.getSendMessageTextArea().getText()};
+            client.writeMessage(new Message(MessageType.SEND_GROUP, toSend));
+        });
     }
 }
