4f60fa861705f686e4bec9e982d5a948478a7f69
gdgenchev <gdgnechev97@gmail.com>
2018-09-26 12:30:33 +0300

    Change set to list due to the need of adding an element while iterating

--- src/bg/chat/client/controller/ReceiverBroadcaster.java
+++ src/bg/chat/client/controller/ReceiverBroadcaster.java
@@ -3,26 +3,24 @@ package bg.chat.client.controller;
 import bg.chat.client.model.Client;
 import bg.chat.common.Message;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ReceiverBroadcaster extends Thread {
     private Client client;
-    private Set<Receiver> receivers;
+    private List<Receiver> receivers;
 
     ReceiverBroadcaster(Client client) {
         this.client = client;
-        receivers = Collections.synchronizedSet(new HashSet<>());
+        receivers = new ArrayList<>();
     }
 
-    synchronized void addReceiver(Receiver receiver) {
+    void addReceiver(Receiver receiver) {
         this.receivers.add(receiver);
     }
 
-    synchronized private void broadcastMessage(Message msg) {
-        for (Receiver receiver : receivers) {
-            receiver.respondToMessage(msg);
+    private void broadcastMessage(Message msg) {
+        for (int i = 0; i < receivers.size(); i++) {
+            receivers.get(i).respondToMessage(msg);
         }
     }
 
