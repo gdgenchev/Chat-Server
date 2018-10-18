a1245d7bba24081a8c0b35bf9b13b0adeb60c2eb
gdgenchev <gdgenchev97@gmail.com>
2018-10-19 00:52:06 +0300

    Add constant for chat history location

--- src/bg/chat/utils/Constants.java
+++ src/bg/chat/utils/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int PORT = 9001;
     public static final int ERROR_MS = 1500;
     public static final String HOST = "localhost";
+    public static final String SAVE_DIRECTORY = "saved/";
 }
