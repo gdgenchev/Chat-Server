93bd7287834e1a56ee0c90b07740973b0df1337e
gdgenchev <gdgenchev97@gmail.com>
2018-10-19 00:47:39 +0300

    Add FileService class for managing chat history

---
+++ src/bg/chat/server/FileService.java
@@ -0,0 +1,53 @@
+package bg.chat.server;
+
+import bg.chat.utils.Constants;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class FileService {
+    static void writeMessageToFile(String fileName, String msg) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(
+                        Constants.SAVE_DIRECTORY
+                                + fileName, true),
+                StandardCharsets.UTF_8))) {
+            writer.write(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void createFile(String fileName) {
+        writeMessageToFile(fileName, "");
+    }
+
+    static String getChatRoomHistory(String fileName) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(Constants.SAVE_DIRECTORY + fileName))))
+        {
+            StringBuilder chatHistory = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                chatHistory.append(line).append("\n");
+            }
+            chatHistory.deleteCharAt(chatHistory.length() - 1); // remove last new line
+            return chatHistory.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    static void deleteFile(String roomName) {
+        try {
+            Files.delete(Paths.get(Constants.SAVE_DIRECTORY + roomName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
